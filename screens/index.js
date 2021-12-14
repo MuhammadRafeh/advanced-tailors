4371672e09091482ccc8c2f9834e0cae9c22ac0e
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-12-15 00:38:57 +0500

    Added Video Correctly!

--- screens/index.js
+++ screens/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { Image, Text, View, StyleSheet } from 'react-native';
+import Video from 'react-native-video';
 
 const index = props => {
+    const player = useRef(null);
     return (
-        <View>
-            <Text>
-                Hi hello
-            </Text>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={{ flex: 0.9, top: -20 }}>
+                <Video source={require('../assets/tailor.mp4')}   // Can be a URL or a local file.
+                    ref={(ref) => {
+                        player.current = ref;
+                    }}
+                    paused={false}                                 // Store reference
+                    repeat={true}
+                    onBuffer={() => { }}                // Callback when 
+                    // onEnd={() => { player.current.refs.node.seek(0) }}
+                    resizeMode={'contain'}
+                    style={styles.backgroundVideo} />
+            </View>
+            <View style={{ flex: 0.1, backgroundColor: 'white', top: -90, borderTopLeftRadius: 20, borderTopRightRadius: 20 }}>
+               <Text>
+                   asd
+               </Text>
+            </View>
         </View>
     );
 }
 
 export default index;
+
+const styles = StyleSheet.create({
+    backgroundVideo: {
+        width: '100%',
+        height: '100%'
+    },
+});
