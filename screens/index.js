3a99f9009b050a42da5735a4504a64c8d216a227
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-12-14 22:31:23 +0500

    Added Index File

---
+++ screens/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const index = props => {
+    return (
+        <View>
+            <Text>
+                Hi hello
+            </Text>
+        </View>
+    );
+}
+
+export default index;
