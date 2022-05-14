070c6569d94a7a2aa4aac818ef695d355ca2ac32
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-05-14 20:03:43 +0500

    Updated Home with Cards!

--- screens/Customer/Home.js
+++ screens/Customer/Home.js
@@ -84,11 +84,58 @@
 
 // React native and others libraries imports
 import React, { Component } from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, ScrollView } from 'react-native';
 
-const Home = () => {
+const Home = props => {
   return (
-    <View />
+    <>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <View style={{ flex: 1, flexDirection: 'row', paddingTop: 10 }}>
+          {moreProducts.map((item, key) => (
+            <View style={{ width: 180, marginHorizontal: 10 }} key={key}>
+              <View style={styles.moreProductImageView}>
+                <Image
+                  style={{ flex: 1 }}
+                  source={{
+                    uri: item.productImage,
+                  }}
+                />
+              </View>
+              <View style={{ marginTop: 8 }}>
+                <Text style={styles.moreProductName}>
+                  {item.productName}
+                </Text>
+                <View style={styles.moreProductPriceView}>
+                  <Text style={styles.moreProductPrice}>
+                    ${item.productPrice}
+                  </Text>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Icon
+                      style={styles.moreProductIcon}
+                      name='heart'
+                      size={18}
+                    />
+                    <Icon
+                      style={styles.moreProductIcon}
+                      name='shopping-bag'
+                      size={18}
+                    />
+                    <Icon
+                      style={styles.moreProductIcon}
+                      name='share'
+                      size={18}
+                    />
+                  </View>
+                </View>
+              </View>
+              <TouchableOpacity style={styles.moreProductBuyButton}>
+                <Text style={styles.moreProductBuyButtonText}>Buy</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </>
   )
 }
 
